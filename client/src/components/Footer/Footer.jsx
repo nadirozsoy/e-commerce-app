96dee2735474a1b32bf1ee32ffd048f6065f7b7b
Nadir Özsoy <thisisnadirozsoy@gmail.com>
2023-02-26 18:08:01 +0300

    Added | Footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,61 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
-    return <div>Footer</div>
+  return (
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h2>Categories</h2>
+          <Link className="link-2">Women</Link>
+          <Link className="link-2">Man</Link>
+          <Link className="link-2">Shoes</Link>
+          <Link className="link-2">Accersories</Link>
+          <Link className="link-2">New Arrivals</Link>
+        </div>
+        <div className="item">
+          <h2>Links</h2>
+          <Link className="link-2">FAQ</Link>
+          <Link className="link-2">Pages</Link>
+          <Link className="link-2">Stores</Link>
+          <Link className="link-2">Compare</Link>
+          <Link className="link-2">Cookies</Link>
+        </div>
+        <div className="item">
+          <h2>About</h2>
+          <span>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab velit
+            veniam quis. Perferendis laborum esse, beatae ad repellat labore
+            velit ab vel cumque assumenda? Beatae sit voluptatum optio qui
+            fugiat.
+          </span>
+        </div>
+        <div className="item">
+          <h2>Contact</h2>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, sed
+            magni. In at accusamus modi officia doloribus facere similique
+            ratione laboriosam libero porro tempora dolore debitis consequuntur
+            explicabo, adipisci recusandae?
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <div className="logo">Rarestore</div>
+          <div className="copyright">
+            &copy; Copyright 2023.All Rights Reserved
+          </div>
+        </div>
+        <div className="right">
+          <img
+            src="/img/payment.png"
+            alt="payment"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,69 +1,119 @@
-import React from "react"
+import React from 'react'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import SearchIcon from '@mui/icons-material/Search'
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'
-import { Link } from "react-router-dom"
-
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-    return (
-        <div className='navbar'>
-            <div className="wrapper">
-                <div className="left">
-                    <div className="item">
-                        <img src="/img/en.png" alt="flag" />
-                        <KeyboardArrowDownIcon />
-                    </div>
-                    <div className="item">
-                        <Link className='link' to="/products/1">USD</Link>
-                        <KeyboardArrowDownIcon />
-                    </div>
-                    <div className="item">
-                        <Link className='link' to="/products/2">Men</Link>
-                    </div>
-                    <div className="item">
-                        <Link className='link' to="/products/3">Women</Link>
-                    </div>
-                    <div className="item">
-                        <Link className='link' to="/products/4">Children</Link>
-                    </div>
-                    <div className="item">
-                        <Link className='link' to="/products/5">Accersories</Link>
-                    </div>
-                </div>
-                <div className="center">
-                    <Link className='link' to="/">RODISTORE</Link>
-                </div>
-                <div className="right">
-                    <div className="item">
-                        <Link className='link' to="/products/6">Homepage</Link>
-                    </div>
-                    <div className="item">
-                        <Link className='link' to="/products/7">About</Link>
-                    </div>
-                    <div className="item">
-                        <Link className='link' to="/products/8">Contact</Link>
-                    </div>
-                    <div className="item">
-                        <Link className='link' to="/products/9">Stores</Link>
-                    </div>
-                    <div className="icons">
-                        <SearchIcon />
-                        <PersonOutlineIcon />
-                        <FavoriteBorderIcon />
-                        <div className="cardIcon">
-                            <ShoppingCartOutlinedIcon />
-                            <span>0</span>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+            <img
+              src="/img/en.png"
+              alt="flag"
+            />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link
+              className="link"
+              to="/products/1"
+            >
+              USD
+            </Link>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link
+              className="link"
+              to="/products/2"
+            >
+              Men
+            </Link>
+          </div>
+          <div className="item">
+            <Link
+              className="link"
+              to="/products/3"
+            >
+              Women
+            </Link>
+          </div>
+          <div className="item">
+            <Link
+              className="link"
+              to="/products/4"
+            >
+              Children
+            </Link>
+          </div>
+          <div className="item">
+            <Link
+              className="link"
+              to="/products/5"
+            >
+              Accersories
+            </Link>
+          </div>
         </div>
-    )
+        <div className="center">
+          <Link
+            className="link"
+            to="/"
+          >
+            RARESTORE
+          </Link>
+        </div>
+        <div className="right">
+          <div className="item">
+            <Link
+              className="link"
+              to="/products/6"
+            >
+              Homepage
+            </Link>
+          </div>
+          <div className="item">
+            <Link
+              className="link"
+              to="/products/7"
+            >
+              About
+            </Link>
+          </div>
+          <div className="item">
+            <Link
+              className="link"
+              to="/products/8"
+            >
+              Contact
+            </Link>
+          </div>
+          <div className="item">
+            <Link
+              className="link"
+              to="/products/9"
+            >
+              Stores
+            </Link>
+          </div>
+          <div className="icons">
+            <SearchIcon />
+            <PersonOutlineIcon />
+            <FavoriteBorderIcon />
+            <div className="cardIcon">
+              <ShoppingCartOutlinedIcon />
+              <span>0</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
